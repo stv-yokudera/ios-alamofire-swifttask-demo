9a5a5919ae77547debb47f03b2852a2ae4dcc089
OkuderaYuki <>
2018-05-03 00:34:46 +0900

    first commit

---
+++ ios-alamofire-swifttask-demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-alamofire-swifttask-demo
+//
+//  Created by OkuderaYuki on 2018/05/03.
+//  Copyright © 2018年 OkuderaYuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
